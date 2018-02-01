36eecd1c2d30452a17c9e709f15912ac53e12944
timstomatos <imshiny808@gmail.com>
2018-02-01 00:06:42 -1000

    1 More

--- loopy_conditions.js
+++ loopy_conditions.js
@@ -8,13 +8,16 @@
  */
 function greaterNumber(a, b) {
 	if (a < b) {
-		return b
+		return b;
 	}
 	if (b < a) {
-		return a
+		return a;
+	}
+	if (a = b){
+		return "Same";
 	}
 }
-console.log(greaterNumber(6,6))	
+console.log(greaterNumber(8,7))	
 
 
 /**
@@ -26,13 +29,14 @@ console.log(greaterNumber(6,6))
  * @param  { Number } n
  * @return { String }
  */
+ var n;
 function stringOfNumbers(n) {
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n.length; i--) {
 		
 	}
-	return n + " ";
+	
 }
-console.log(stringOfNumbers(5));
+console.log(stringOfNumbers(6));
 
 /**
  * Create a function called 'sumOfArray'.
@@ -42,16 +46,13 @@ console.log(stringOfNumbers(5));
  * @param  { array } arr
  * @return { Number }
  */
- var numberSum = [1,2,3,4,5,6,7,8,9,10]
-function sumOfArray(numberTotal) {
-	console.log(numberTotal);
 
-	for (i=0; numberSum.length; i++) {
-		console.log(numberTotal[i]);
-	}
-	return 10 
+var numberSum = [1,2,3,4,5,6,7,8,9,10].reduce(sumOfArray, 0);
+function sumOfArray(a, b) {
+	return a + b;	
 }
-console.log(sumOfArray(numberSum));
+console.log(numberSum);
+
 /**
  * Create a function called 'getEvens'.
  * This function will take in a parameter that is an array.
@@ -60,7 +61,13 @@ console.log(sumOfArray(numberSum));
  * @return { array }
  */
 
-
+var even = [1,2,3,4,5,6,7,8,9,10];
+function getEvens(even){
+	for (var i = 2; i <= even.length; i = i + 2) {
+		console.log(i);
+	}
+}
+console.log(getEvens(even));
 /**
  * Create a function called 'getOdds'.
  * This function will take in a parameter that is an array.
@@ -69,14 +76,21 @@ console.log(sumOfArray(numberSum));
  * @return { array }
  */
 
+var odd = [1,2,3,4,5,6,7,8,9,10];
+function getOdds(odds){
+	for (var i = 1;  i <= odd.length;  i =  i + 2) {
+		console.log(i);
+	}
+ }
+console.log(getOdds(odd));
 
-/**
+/**multiply', 'divide'.
+ * Your task is to do a mathematical operation on the 2 numbers bas
  * Create a function called 'calculate'.
- * This function will take in 3 parameters:
+ * This function will take in 3 parameters:ed on the third param
  * The first parameter is a number.
  * The second parameter is a number.
- * The third param is a string that will be 'add', 'subtract', 'multiply', 'divide'.
- * Your task is to do a mathematical operation on the 2 numbers based on the third parameter.
+ * The third param is a string that will be 'add', 'subtract', 'eter.
  * The function should return the string "Invalid operator" if the operator is
  * not 'add', 'subtract', 'multiply', 'divide'.
  * @param  { Number } int1
@@ -84,3 +98,42 @@ console.log(sumOfArray(numberSum));
  * @param  { String } operator ('add', subtract, 'multiply', 'divide')
  * @return { Number/String }
  */
+/*var operator = ["add", "subtract", "divide", "multiply"]
+function calculate(num1,num2, operator){
+	if ("add") {
+		return num1 + num2;
+	}
+	if ("subtract"){
+		return num1 - num2;
+	}
+	if ("multiply") {
+		return num1 * num2;
+	}
+	if ("divide") {
+		return num1 / num2;
+	}
+	else {
+		return "Invalid operator";
+	}
+}
+console.log(calculate(100,200, "subtract"));*/
+
+function calculate(operator, num1, num2){
+	if (operator == "add") {
+		output = num1 + num2;
+	}
+	if (operator == "subtract"){
+		output = num1 - num2;
+	}
+	if (operator == "multiply") {
+		 output = num1 * num2;
+	}
+	if (operator == "divide") {
+		output = num1 / num2;
+	}
+	else {
+		output = "Invalid operator";
+	}
+}
+calculate("multiply", 20, 5);
+console.log(output);
